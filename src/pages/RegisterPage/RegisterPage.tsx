a4cb6552c2517d5dcfe47db0c41ad967e8b14ef8
Daniil Sivachenko <daniil.s.2003@gmail.com>
2025-01-12 19:55:57 +0300

    RegisterPage component

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,8 +1,34 @@
-export interface RegisterFormProps {
-	onSubmit: (
-		name: string,
-		email: string,
-		password: string,
-		confirmPassword: string
-	) => void;
-}
+import React from 'react';
+import Layout from '../../containers/Layout/Layout';
+import Section from '../../containers/Section/Section';
+import Card from '../../containers/Card/Card';
+import RegisterForm from '../../screens/RegisterForm/RegisterForm';
+
+const handleRegisterSubmit = (
+	name: string,
+	email: string,
+	password: string,
+	confirmPassword: string
+) => {
+	console.log('Register submitted:', {
+		name,
+		email,
+		password,
+		confirmPassword,
+	});
+	// Handle register logic
+};
+
+const RegisterPage: React.FC = () => {
+	return (
+		<Layout>
+			<Section>
+				<Card>
+					<RegisterForm onSubmit={handleRegisterSubmit} />
+				</Card>
+			</Section>
+		</Layout>
+	);
+};
+
+export default RegisterPage;
